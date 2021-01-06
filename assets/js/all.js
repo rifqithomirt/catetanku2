c0873c9db45028f5f9492be86a0ee7fe8a5232aa
rifqithomirt <rifqi.arti.rt@gmail.com>
2021-01-07 05:37:10 +0700

    lagi

---
+++ assets/js/all.js
@@ -0,0 +1,4 @@
+const demo = document.querySelector('#body-list-item');
+const psx = new PerfectScrollbar(demo);
+const demo2 = document.querySelector('#cart-item');
+const ps = new PerfectScrollbar(demo2);
